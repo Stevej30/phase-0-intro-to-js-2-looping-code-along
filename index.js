1721e0fa6708429cd505213dd33661ea9b77bc73
Stevan Jacob <stevanjacob@gmail.com>
2021-09-13 08:22:50 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(name, event){
+    const newArr = [];
+    for (let i = 0; i < name.length; i ++){
+    newArr.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    console.log(newArr);
+    return newArr;
+    
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday")
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num--)
+    }
+}
+
+countDown(10);
